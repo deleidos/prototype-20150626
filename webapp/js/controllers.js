9dd330cb7037304bc62dc8bc72c8e193bf1ef52e
unknown <martzth@F0RS9S1.cic.saic.com>
2015-06-23 14:07:28 -0400

    Developed ajax call for state data

--- webapp/js/controllers.js
+++ webapp/js/controllers.js
@@ -35,8 +35,10 @@
      * @param $scope
      * @constructor
      */
-    var MapController = function($scope, $http) {
-        function init() {
+    var MapController = function($scope, $http, $q) {
+        var type = false;
+
+        function search_by_name() {
             angular.extend($scope, {
                 center: {
                     lat: 39,
@@ -47,11 +49,12 @@
             var all_locations = [];
             $http.get("data/us-states.json").success(function(response, status) {
                 state_location = response.features
+                //$http.get("http://ec2-54-147-248-210.compute-1.amazonaws.com:8080/mongorest/mongo/query?host=10.153.211.57&database=dbname&collection=fda_enforcement&filter={%22openfda.substance_name.0.0%22:%22CANDESARTAN%20CILEXETIL%22}").success(function (data, status) {
                 $http.get("data/state-response.json").success(function (data, status) {
-                recall_location = data.states
+                recall_location = data.results[0].recall_area
                     angular.forEach(recall_location, function (recall_locale) {
                         angular.forEach(state_location, function (state_locale) {
-                            if (recall_locale == state_locale.properties.name) {
+                            if (recall_locale == state_locale.properties.name || recall_locale == "Nationwide") {
                                 all_locations.push(state_locale);
                             }
                         });
@@ -72,7 +75,16 @@
                 }
             });
         }
-        init()
+
+        function search_by_state() {
+
+        }
+
+        if(type) {
+            search_by_name();
+        } else {
+            search_by_state();
+        }
     };
     angular.module('App').controller('MapController', ["$scope", "$http", MapController]);
 
